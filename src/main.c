85ffc195ffb843705f7f34544f98983b6a37fc4a
gabriel costa <gabriel.costa@ua.pt>
2024-12-03 14:42:11 +0000

    changed toggle to set pin value

--- src/main.c
+++ src/main.c
@@ -53,7 +53,7 @@ static void uart_cb(const struct device *dev, struct uart_event *evt, void *user
             else if (buffer_idx>0 && received_char == '#'){
                 buffer[buffer_idx++] = received_char;
 				printk("#\n");
-				printk("%s\n",buffer);
+				// printk("%s\n",buffer);
 				if(buffer[1] == 'P'){	// PC -> Microcontroller communication
                     if(buffer[2] == 'O' && buffer[5]=='#'){ // turn on a LED
                         if(buffer[3] == '1'){	// LED 1
@@ -65,11 +65,22 @@ static void uart_cb(const struct device *dev, struct uart_event *evt, void *user
                                 gpio_pin_set_dt(&led0,0);
                             }
                         } else if(buffer[3] == '2'){	// LED 2
-                            if(buffer[4] == '1') // turn on
-                                gpio_pin_toggle_dt(&led1);
+                            if(buffer[4] == '1' ){ // turn on
+                                // gpio_pin_toggle_dt(&led0);
+                                // printk("toggling led0\n");
+                                gpio_pin_set_dt(&led1,1);
+                            } else if(buffer[4] == '0'){
+                                gpio_pin_set_dt(&led1,0);
+                            }
                         } else if(buffer[3] == '3'){	// LED 3
-                            if(buffer[4] == '1') // turn on
-                                gpio_pin_toggle_dt(&led2);
+                            if(buffer[4] == '1' ){ // turn on
+                                // gpio_pin_toggle_dt(&led0);
+                                // printk("toggling led0\n");
+                                gpio_pin_set_dt(&led2,1);
+                            } else if(buffer[4] == '0'){
+                                gpio_pin_set_dt(&led2,0);
+                            }
+                            
                         }
                         
                     }
@@ -135,7 +146,7 @@ void task0(void *argA, void *argB, void *argC) {
     // k_tid_t task_id = *(k_tid_t *)id_ptr; // Retrieve task ID
     while (1) {
         k_thread_suspend(thread0);
-        printk("Task0 executing %d\n",thread0); // Simulate task behavior
+        // printk("Task0 executing %d\n",thread0); // Simulate task behavior
     }
 }
 
@@ -143,7 +154,7 @@ void task1(void *argA, void *argB, void *argC) {
     // k_tid_t task_id = *(k_tid_t *)id_ptr; // Retrieve task ID
     while (1) {
         k_thread_suspend(thread1);
-        printk("Task1 executing %d\n",thread1); // Simulate task behavior
+        // printk("Task1 executing %d\n",thread1); // Simulate task behavior
         // k_msleep(TICK_MS); // Simulate work
     }
 }
@@ -152,7 +163,7 @@ void task2(void *argA, void *argB, void *argC) {
     // k_tid_t task_id = *(k_tid_t *)id_ptr; // Retrieve task ID
     while (1) {
         k_thread_suspend(thread2);
-        printk("Task2 executing %d\n",thread2); // Simulate task behavior
+        // printk("Task2 executing %d\n",thread2); // Simulate task behavior
         // k_msleep(TICK_MS); // Simulate work
     }
 }
